791a915f0edc075856bdfb8d1b2447c6c74cad34
[dyad] <git@dyad.sh>
2026-03-18 01:54:52 +0200

    Reverted all changes back to version 566e5119c35b8c6bfc74c73365c8b89167575f89

--- src/App.tsx
+++ src/App.tsx
@@ -4,16 +4,6 @@ import { TooltipProvider } from "@/components/ui/tooltip";
 import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
 import { BrowserRouter, Routes, Route } from "react-router-dom";
 import Index from "./pages/Index";
-import Resorts from "./pages/Resorts";
-import ResortDetail from "./pages/ResortDetail";
-import Hotels from "./pages/Hotels";
-import Packages from "./pages/Packages";
-import Activities from "./pages/Activities";
-import Insurance from "./pages/Insurance";
-import VideoBonus from "./pages/VideoBonus";
-import QRActivation from "./pages/QRActivation";
-import ResortComparison from "./pages/ResortComparison";
-import Guides from "./pages/Guides";
 import NotFound from "./pages/NotFound";
 
 const queryClient = new QueryClient();
@@ -26,16 +16,7 @@ const App = () => (
       <BrowserRouter>
         <Routes>
           <Route path="/" element={<Index />} />
-          <Route path="/resorts" element={<Resorts />} />
-          <Route path="/resort/:slug" element={<ResortDetail />} />
-          <Route path="/hotels" element={<Hotels />} />
-          <Route path="/packages" element={<Packages />} />
-          <Route path="/activities" element={<Activities />} />
-          <Route path="/insurance" element={<Insurance />} />
-          <Route path="/video-bonus" element={<VideoBonus />} />
-          <Route path="/redeem/:trip_id" element={<QRActivation />} />
-          <Route path="/compare/resorts" element={<ResortComparison />} />
-          <Route path="/guides" element={<Guides />} />
+          {/* ADD ALL CUSTOM ROUTES ABOVE THE CATCH-ALL "*" ROUTE */}
           <Route path="*" element={<NotFound />} />
         </Routes>
       </BrowserRouter>
