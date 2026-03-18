e2ae3284a287ada15360f3d160b742aa6cbd9e97
[dyad] <jamalmamaev@gmail.com>
2026-03-18 02:54:44 +0200

    [dyad] Исправлены пути в index.html, обновлена инициализация React и добавлены базовые компоненты уведомлений. - wrote 4 file(s)

--- src/main.tsx
+++ src/main.tsx
@@ -1,5 +1,20 @@
+import React from "react";
 import { createRoot } from "react-dom/client";
-import App from "./App.tsx";
+import App from "./App";
 import "./globals.css";
 
-createRoot(document.getElementById("root")!).render(<App />);
+console.log("App is initializing...");
+
+const container = document.getElementById("root");
+
+if (!container) {
+  console.error("Failed to find the root element");
+} else {
+  const root = createRoot(container);
+  root.render(
+    <React.StrictMode>
+      <App />
+    </React.StrictMode>
+  );
+  console.log("App rendered successfully");
+}
